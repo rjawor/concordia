201b07c6d2c0b8886168b263a3bc848a2855b981
rjawor <rjawor@amu.edu.pl>
2013-12-07 00:00:11 +0100

    reorganized tests

--- concordia-runner-large.sh
+++ concordia-runner-large.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Decompressing test file"
+
+bunzip2 --keep prod/resources/text-files/large.txt.bz2
+
 echo "Running Concordia"
 
 rm prod/resources/temp/*
@@ -7,3 +11,5 @@ rm prod/resources/temp/*
 ./build/concordia-console/concordia-console -c prod/resources/concordia-config/concordia.cfg -g
 ./build/concordia-console/concordia-console -c prod/resources/concordia-config/concordia.cfg -s "drawn from his own" -n
 ./build/concordia-console/concordia-console -c prod/resources/concordia-config/concordia.cfg -s "it is" -n
+
+rm prod/resources/text-files/large.txt

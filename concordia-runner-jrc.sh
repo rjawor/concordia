b298f4160e18d15477ff0e858e35be824fb22632
rjawor <rjawor@amu.edu.pl>
2014-02-20 13:11:31 +0100

    smaller test files

--- concordia-runner-jrc.sh
+++ concordia-runner-jrc.sh
@@ -2,17 +2,17 @@
 
 echo "CONCORDIA RUNNER: Decompressing test file"
 
-bunzip2 --keep prod/resources/text-files/jrc.txt.bz2
+xz -dk prod/resources/text-files/jrc_smaller.txt.xz
 
 echo "CONCORDIA RUNNER: Running Concordia"
 
 rm prod/resources/temp/*
 
 echo "CONCORDIA RUNNER: reading from file"
-./build/concordia-console/concordia-console -c prod/resources/concordia-config/concordia.cfg -r prod/resources/text-files/jrc.txt
+./build/concordia-console/concordia-console -c prod/resources/concordia-config/concordia.cfg -r prod/resources/text-files/jrc_smaller.txt
 echo "CONCORDIA RUNNER: searching for pattern: \"Parlamentu Europejskiego\""
 ./build/concordia-console/concordia-console -c prod/resources/concordia-config/concordia.cfg -s "Parlamentu Europejskiego" -n
 echo "CONCORDIA RUNNER: searching for pattern: \"Dostęp do zatrudnienia\""
 ./build/concordia-console/concordia-console -c prod/resources/concordia-config/concordia.cfg -s "Dostęp do zatrudnienia" -n
 
-rm prod/resources/text-files/jrc.txt
+rm prod/resources/text-files/jrc_smaller.txt
